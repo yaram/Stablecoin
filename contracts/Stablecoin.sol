89b5ade1af631e1cd0ab3bdb8f04d6f5410af0e4
Yaram <pootisspencerherenao@gmail.com>
2019-11-20 20:55:10 +1100

    Add openzepplin contracts

--- contracts/Stablecoin.sol
+++ contracts/Stablecoin.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.5.2;
 
-contract Stablecoin {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract Stablecoin is ERC20Detailed {
+    constructor() ERC20Detailed("Stablecoin", "SBC", 18) public {
+
+    }
 }
